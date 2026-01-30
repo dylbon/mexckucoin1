d764f0b9e0c6d0203dfdbe3a01ebc5121953ddd1
dylbon <dylanbonnici16@gmail.com>
2026-01-30 21:54:01 +0100

    Update arb_scanner.py

--- arb_scanner.py
+++ arb_scanner.py
@@ -24,7 +24,7 @@ BLACKLIST = {
     'PIPE', 'FLOW',
     'TAKE', 'CHECK', 'SONIC', 'PORTAL', 'ART', 'UNITE', 'AIO',
     'L3', 'RVV', 'U2U',
-    'DUCK', 'SAMO','SKR','NODL', 'GWEI', 'U','TEL','BILLY','XNAP'
+    'DUCK', 'SAMO','SKR','NODL', 'GWEI', 'U','TEL','BILLY','XNAP','EPT'
 }
 
 SYMBOL_MAP = {
